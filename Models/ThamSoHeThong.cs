c44bcc2c59610ac2dff09d65c70d5f3e2d52f0e5
Huy ăn rồi ngủ <lechanhuy.sd1@gmail.com>
2025-09-22 22:50:06 +0700

    Initial commit

---
+++ Models/ThamSoHeThong.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QL_ThuVien.Models;
+
+public partial class ThamSoHeThong
+{
+    public int Id { get; set; }
+
+    public string Ten { get; set; } = null!;
+
+    public string GiaTri { get; set; } = null!;
+}
