6fd9eb7eba6e89792b1b045c20407caf54f72967
Huy <lechanhuy.sd1@gmail.com>
2025-10-25 20:59:01 +0700

    Delete Font_end directory

--- Font_end/view/admin_cp/js/create.js
+++
@@ -1,8 +0,0 @@
-fetch("pages/create.html")
-  .then(res => res.text())
-  .then(data => {
-    document.getElementById("content").innerHTML = data;
-  })
-  .catch(err => {
-    console.error("Không thể tải nội dung từ pages/create.html");
-  });
