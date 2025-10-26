88e37e58ec1ee2d42dc85a10fbf6274f93232a0a
Huy <lechanhuy.sd1@gmail.com>
2025-10-27 00:54:12 +0700

    Delete QLTL directory

--- QLTL/Models/NhomSach.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace QLTV_Backend.Models
-{
-    public class NhomSach
-    {
-        public int Id { get; set; }
-        public string MaNhom { get; set; }
-        public string TenNhom { get; set; }
-    }
-}
